717e836a1f4306340654af5bd55676a2c9341aa3
Matias Rodriguez <mayutefracran@gmail.com>
2024-01-26 10:46:13 +0000

    v0.1.8

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,60 +1,21 @@
-'use client';
-
 import React from 'react'
-import { useForm } from 'react-hook-form'
-
-import { Button, Label, TextInput } from 'flowbite-react';
+import Register from "../components/RegisterComponent"
 
 function RegisterPage() {
-
-  const { register, handleSubmit } = useForm()
-
   return (
     <>
-      <form onSubmit={handleSubmit((values) => {
-        console.log(values)
-      })} className="flex max-w-md flex-col gap-4">
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="name2" value="Nombre" />
-          </div>
-          <TextInput type="text" {...register('name', { require: true })} className="mb-2 block" placeholder='Juan' />
-        </div>
-
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="lastname2" value="Apellido" />
-          </div>
-          <TextInput type="text" {...register('lastname', { required: true })} placeholder='Pérez' />
-        </div>
-
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="phone2" value="Telefono" />
-          </div>
-          <TextInput type='number' {...register('phone', { required: true })} />
-        </div>
-
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="email2" value="Email" />
-          </div>
-          <TextInput type="email" {...register('email', { required: true })} placeholder='mejor@precio.com' />
-        </div>
-
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="password2" value="Your password" />
-          </div>
-          <TextInput type="password" {...register('password', { required: true })} placeholder='******' minLength={8} />
-        </div>
-
-
-        <Button type='submit'>
-          Registrarse
-        </Button>
-      </form>
-    </>
+            <div className="flex justify-end">
+                <div className="hidden md:block">
+                    <Sidebar />
+                </div>
+                <div className='w-full'>
+                    <div className="md:hidden">
+                        <Navbar />
+                    </div>
+                    <Register/>
+                </div>
+            </div>
+        </>
   )
 }
 
